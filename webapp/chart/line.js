cf63696df84bc62a55ba82d6d92c7875fe855c9b
Miu Narukawa <univ0802miu@gmail.com>
2022-10-26 17:52:53 +0900

    整理した

---
+++ webapp/chart/line.js
@@ -0,0 +1,70 @@
+// (function () {
+//     'use strict'
+//     // 棒グラフ
+//     var type = 'bar';
+    
+//     var data = {
+//         labels: [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+//         datasets: [{
+//             label: '@taguchi',
+//             data: [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30],
+//             borderColor: 'red',
+//             // 枠線をなくしたい
+//             borderWidth: 0,
+//             fill: false,
+//             // ポイントのマーク
+//             pointStyle: 'rect'
+//         }]
+//     };
+//     // 0から始まるグラフにしたい
+//     var options = {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     // suggestedにするといい感じに調整してくれる
+//                     suggestedMin: 0h,
+//                     suggestedMax: 8h,
+//                     // 2hきざみにする
+//                     stepsize: 2h
+//                 }
+//             }]
+//         },
+//         // Chart全体の設定
+//         // デフォルトでアニメーションがついているが、それをやめたいとき
+//         animation: {
+//             duration: 0
+//         },
+//         // 凡例の設定
+//         legend: {
+//             position: 'bottom'
+//         }
+//     };
+//     let ctx = document.getElementById('linegraph').getContext('2d');
+//     let myChart = new Chart(ctx, {
+//         type: type,
+//         data: data,
+//         options: options
+//     });
+// })();
+var ctx = document.getElementById('ex_chart');
+var data = {
+    labels: ["令和元年", "令和2年", "令和3年", "令和4年", "令和5年"],
+    datasets: [{
+        label: '得点',
+        data: [880, 740, 900, 520, 930],
+        backgroundColor: 'rgba(255, 100, 100, 1)'
+    }]
+};
+var options = {
+    scales: {
+        y:{
+            min: 300
+            //beginAtZero: true
+        }
+    }
+};
+var ex_chart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: options
+});

ea81ca0234513c696d7e2b93378531e919c1ab47
Miu Narukawa <univ0802miu@gmail.com>
2022-10-27 15:17:16 +0900

    棒グラフを表示したけど、関数名が同じで円グラフが消えた

--- webapp/chart/line.js
+++ webapp/chart/line.js
@@ -1,6 +1,35 @@
 'use strict';
 
-
+window.onload = function () {
+    let context = document.querySelector("#line_chart").getContext('2d')
+    new Chart(context, {
+        // 実際に表示したいグラフのデータ
+        type: 'bar',
+        data: {
+            labels: ['', '2', '', '4', '', '6','','8', '', '10', '', '12','', '14', '', '16', '', '18','','20', '', '22', '', '24','', '26', '', '28','','30'],
+            datasets: [{
+                // label: "日本の人口推移",
+                data: [2, 3, 2.5, 4, 3, 6, 5,6.6, 7.5, 7, 3, 5, 6, 5, 7, 3, 5, 4, 2, 5, 4, 2, 6, 5, 7, 6, 5, 5, 4,6],
+                backgroundColor:'#4542f5'
+            }],
+        },
+        options: {
+            // responsive: false
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMax: 8,
+                        suggestedMin: 0,
+                        stepSize: 2,
+                        callback: function(value, index, values){
+                            return  value +  'h'
+                          }
+                    }
+                }]
+            }
+        }
+    })
+}
 
 
 

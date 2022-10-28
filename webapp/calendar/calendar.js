d352aaf7186ee5524882eac2b5d4f9d705f1be4b
Miu Narukawa <univ0802miu@gmail.com>
2022-10-28 13:40:18 +0900

    カレンダーをN予備通りにした

--- webapp/calendar/calendar.js
+++ webapp/calendar/calendar.js
@@ -1,17 +1,20 @@
 'use strict';
 
 console.clear();
+
 {
     const today = new Date();
     // 日付を固定
     let year = today.getFullYear();
-    let month = today.getMonth();
+    let month = today.getMonth(); //5月
+
     //前月の日付を取得
     function getCalenderHead() {
         const dates = [];
         const d = new Date(year, month, 0).getDate()
         //今月の 1 日のオブジェクトが、週の何日目かを取得
         const n = new Date(year, month, 1).getDay();
+
         for (let i = 0; i < n; i++) {
             //30
             //29,30
@@ -24,8 +27,48 @@ console.clear();
                 isDisabled: true,
             });
         }
+
         return dates;
     }
+
+    // カレンダーの日付の本体
+    function getCalenderBody() {
+        const dates = []; //date日付、 day曜日
+        const lastDate = new Date(year, month + 1, 0).getDate();
+
+        for (let i = 1; i <= lastDate; i++) {
+            // オブジェクトのプロパティにする
+            dates.push({
+                dates: i,
+                isToday: false,
+                isDisabled: false,
+            });
+        }
+
+        //year が today の getFullYear() と同じ、なおかつ、 month が today の getMonth() と同じだったら、今日の日付を太字にしたい
+        if (year === today.getFullYear() && month === today.getMonth()) {
+            //今日の日付を太字にする
+            dates[today.getDate() - 1].isToday = true;
+        }
+
+        return dates;
+    }
+
+    function getCalenderTail() {
+        const dates = [];
+        const lastDay = new Date(year, month + 1, 0).getDay();
+
+        for (let i = 1; i < 7 - lastDay; i++) {
+            dates.push({
+                date: i,
+                isToday: false,
+                isDisabled: true,
+            });
+        }
+
+        return dates;
+    }
+
 
     function clearCalendar() {
         //createCalender() するたびに tbody の中身をクリア
@@ -35,13 +78,15 @@ console.clear();
             tbody.removeChild(tbody.firstChild);
         }
     }
+
     function renderTitle() {
         //月が 1 桁のときは最初に 0 を足す
         // padStart() は文字列にしか使えないので String() でいったん文字列にしてあげて、 padStart() を使う
-        const title = `${year}/${String(month + 1).padStart(2, 0)}`;   // 2桁で表示してね、それに満たなかったら 0 の文字列で埋めてね
+        const title = `${year}年${String(month + 1).padStart(2, '0')}月`;  // 2桁で表示してね、それに満たなかったら 0 の文字列で埋めてね
         document.getElementById('title').textContent = title;
     }
-    function renderWeeks(){
+
+    function renderWeeks() {
         const dates = [
             //それぞれの関数をよびだす
             //全ての要素を１つの配列の中で展開するのでスプレッド構文を使う
@@ -60,6 +105,7 @@ console.clear();
             //dates から 7 日分のデータを取るには、 splice() を使って先頭から 7 個分を削除しつつ取り出してね
             weeks.push(dates.splice(0, 7));
         }
+
         //weeksを使ってHTMLの方に描画していく
         //週ごとに処理をしていきたいので forEach() を回してあげる
         //取り出した配列を week としつつ、次の処理をしてね
@@ -69,6 +115,7 @@ console.clear();
             //取り出した要素をdateにしつつ次の処理をしてねと書く
             week.forEach(date => {
                 const td = document.createElement('td');
+                
                 //textContent プロパティを date の date プロパティにしてあげれば、日付が入る
                 td.textContent = date.date;
                 //isToday プロパティが true だったら today クラスをつけるようにしてあげる
@@ -81,16 +128,33 @@ console.clear();
                 }
                 // td 要素ができたので、 tr 要素の子要素の末尾に追加
                 tr.appendChild(td);
+
+                //追加
+                td.addEventListener('click', () => {
+            
+                    const yearMonth = document.getElementById('title');
+                    const selectedDate = yearMonth.innerHTML+td.innerHTML+'日';
+                    console.log(selectedDate)
+                    const inputDate = document.getElementById('date');
+                    inputDate.value = selectedDate;
+                    const modalCalendar = document.querySelector('.modal_calendar');
+                    const modalMain = document.querySelector('.modal_main');
+                    modalCalendar.style.display = 'none';
+                    modalMain.style.display = 'block';
+        
+                })
             });
             document.querySelector('tbody').appendChild(tr);
-        });
+        })
     }
+
     function createCalender() {
         //clearCalendar,renderTitle,renderWeeksをよびだす
         clearCalendar();
         renderTitle();
         renderWeeks();
     }
+
     //クリックしたら次の処理をしてねとかく
     document.getElementById('prev').addEventListener('click', () => {
         month--;
@@ -98,51 +162,26 @@ console.clear();
             year--;
             month = 11;
         }
+
         createCalender()
-    });
-    document.getElementById('today').addEventListener('click', () => {
-        year = today.getFullYear();
-        month = today.getMonth();
-        createCalender()
-    });
+    })
+
+    // document.getElementById('today').addEventListener('click', () => {
+    //     year = today.getFullYear();
+    //     month = today.getMonth();
+    //     createCalender()
+    // });
+
     document.getElementById('next').addEventListener('click', () => {
         month++;
         if (month > 11) {
             year++;
             month = 0;
         }
-        createCalender()
+
+        createCalendar();
     });
-    // カレンダーの日付の本体
-    function getCalenderBody() {
-        const dates = []; //date日付、 day曜日
-        const lastDate = new Date(year, month + 1, 0).getDate();
-        for (let i; i <= lastDate; i++) {
-            // オブジェクトのプロパティにする
-            dates.push({
-                dates: i,
-                isToday: false,
-                isDisabled: false,
-            });
-        }
-        //year が today の getFullYear() と同じ、なおかつ、 month が today の getMonth() と同じだったら、今日の日付を太字にしたい
-        if (year === today.getFullYear() && month === today.getMonth()) {
-            //今日の日付を太字にする
-       dates[today.getDate.getDate() -1].isToday =true;
-        }
-        return dates;
-    }
-    function getCalenderTail() {
-        const dates = [];
-        const lastDay = new Date(year, month + 1, 0).getDay();
-        for (let i = 1; i < - lastDay; i++) {
-            dates.push({
-                date: i,
-                isToday: false,
-                isDisabled: true,
-            });
-        }
-        return dates;
-    }
-    getCalenderHead();
+
+    createCalender();
+
 }
--- webapp/chart/dounat.js
+++ webapp/chart/dounat.js
@@ -1,20 +1,25 @@
- 'use strict';
-   
- //学習言語ドーナツ
- window.onload = function () {
-    let context = document.querySelector("#language_doughnut").getContext('2d')
-    new Chart(context, {
-      type: 'doughnut',
-      data: {
-        labels: ["HTML", "CSS", "JavaScript", "PHP", "Laravel","SQL","SHELL","情報システム（その他）"],
-        datasets: [{
-          data: [60, 20, 15, 10, 5]
-        }]
-      },
-      options: {
-        responsive: false,
-      }
-    });
-  }
+'use strict';
 
-   
+//学習言語ドーナツ
+window.onload = function () {
+  let context = document.querySelector("#language_doughnut").getContext('2d')
+  new Chart(context, {
+    type: 'doughnut',
+    data: {
+      labels: ["HTML", "CSS", "JavaScript", "PHP", "Laravel", "SQL", "SHELL", "情報システム（その他）"],
+      datasets: [{
+        backgroundColor: [
+          "#BB5179", //まだ
+          "#2d5591",
+          "",
+          "#3C00FF"
+        ],
+        data: [60, 20, 15, 10, 5]
+      }]
+    },
+    options: {
+      responsive: false,
+    }
+  });
+}
+
